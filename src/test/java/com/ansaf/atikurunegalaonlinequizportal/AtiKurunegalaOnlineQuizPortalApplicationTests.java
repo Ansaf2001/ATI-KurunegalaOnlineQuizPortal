e126ba0471ff795c3d0d027d306bf420e4e6f88c
Ansaf2001 <ansafansar2001@gmail.com>
2024-03-25 12:20:12 +0530

    Day-1

---
+++ src/test/java/com/ansaf/atikurunegalaonlinequizportal/AtiKurunegalaOnlineQuizPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansaf.atikurunegalaonlinequizportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiKurunegalaOnlineQuizPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
